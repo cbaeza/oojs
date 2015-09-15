fc3076e5abaaca0c88554510392f873ed11c8c15
cbaeza <carlos.baeza@posteo.de>
2015-09-15 23:20:00 +0200

    add pop up manager

---
+++ src/PopUpManager.js
@@ -0,0 +1,14 @@
+var PopUpManager;
+
+PopUpManager = (function() {
+	PopUpManager.prototype.options = {};
+
+	function PopUpManager(options) {
+		this.options = options;
+	}
+
+	PopUpManager.prototype.init = function() {};
+
+	return PopUpManager;
+
+})();
