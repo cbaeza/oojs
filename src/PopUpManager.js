07fc2cf397921898eb3c337e34937400d7ebde16
carlos.baeza <carlos.baeza@idealo.de>
2015-12-18 17:05:38 +0100

    add listener for click event

--- src/PopUpManager.js
+++ src/PopUpManager.js
@@ -16,6 +16,20 @@ $(function () {
 		 */
 		PopUpManager.prototype.init = function () {
 			console.log("init PopUpManager!");
+			var c = this,
+				$button = $('#myButton');
+
+			console.log($button.text());
+			if( $button !== undefined){
+				//console.log('set click event');
+				$button.click(function(event){
+						event.preventDefault();
+						event.stopPropagation();
+						console.log('button click');
+						alert($button.attr('data-button-click'));
+					}//handler for click event
+				);//click
+			}//if
 		};// init
 
 		return PopUpManager;
